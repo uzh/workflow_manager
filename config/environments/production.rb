96049c4c3c0a7412e64e6be1e1670e1705a80bc7
Masaomi Hatakeyama <masaomi.hatakeyama@gmail.com>
2020-03-24 13:54:37 +0100

    Updated default config/environments/production.rb with redis.conf

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -6,6 +6,7 @@ WorkflowManager::Server.configure do |config|
   config.db_dir = 'dbs'
   config.interval = 30
   config.resubmit = 0
+  config.redis_conf = "config/environments/redis.conf"
   config.cluster = WorkflowManager::FGCZCluster.new('FGCZCluster')
 end
 
