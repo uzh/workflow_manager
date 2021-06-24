16504fb9b1fdffab7525956197450965170ed531
Masaomi Hatakeyama <masaomi.hatakeyama@uzh.ch>
2021-06-24 10:55:26 +0200

    test/job_list.rb

--- test/job_list.rb
+++ test/job_list.rb
@@ -1,13 +1,13 @@
 #!/usr/bin/env ruby
 # encoding: utf-8
-# Version = '20210618-154525'
+# Version = '20210624-105449'
 
 
 require 'redis'
 db0 = Redis.new(port: 6380, db: 0)
 db1 = Redis.new(port: 6380, db: 1)
 db2 = Redis.new(port: 6380, db: 2)
-puts "db0"
+puts "db0, status DB"
 puts ["JobID", "Status"].join("\t")
 db0.keys.sort.each do |key|
   value = db0.get(key)
@@ -15,14 +15,14 @@ db0.keys.sort.each do |key|
 end
 
 puts
-puts "db1"
+puts "db1, log DB"
 db1.keys.sort.each do |key|
   value = db1.get(key)
   puts [key, value].join("\t")
 end
 
 puts
-puts "db2"
+puts "db2, status DB2, project specific"
 db2.keys.sort.each do |key|
   value = db2.get(key)
   puts [key, value].join("\t")
